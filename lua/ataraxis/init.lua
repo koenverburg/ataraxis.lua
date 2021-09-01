6a2258e46d18b1ce2676ed15f88390e631afc536
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-09-01 10:20:38 -0300

    fix: replace window names on status bar with dashes

--- lua/ataraxis/init.lua
+++ lua/ataraxis/init.lua
@@ -9,6 +9,10 @@ function M.setup(custom_options)
 
     options.set(user_options)
 
+    -- replace window names on status bar with "─"
+    vim.o.statusline = '─'
+    vim.o.fillchars = 'stlnc:─'
+
     add_pads()
     autocommands()
 end
