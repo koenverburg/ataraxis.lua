49b63273349b70adaf9cd4493300b7e3bb64a04b
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-09-29 15:33:32 -0300

    refactor: move all autocommands to a `augroup`
    By wrapping all the autocommands used by this plugin in a `augroup`, we
    prevent them from being overwritten by local `autocmd!` that listen
    to the same event, and also, avoids duplication when sourcing your
    (neo)vim config file multiple times, thus making it run slower due to
    executing the same commands over and over

--- lua/ataraxis/autocommands.lua
+++ lua/ataraxis/autocommands.lua
@@ -2,16 +2,19 @@ require 'ataraxis.quit_buffer_or_nvim'
 require 'ataraxis.adjust_pads'
 
 local function autocommands()
-    -- prevent focusing on pads
-    vim.cmd('autocmd! BufEnter leftbuffer wincmd l')
-    vim.cmd('autocmd! BufEnter rightbuffer wincmd h')
+    -- prevent focusing on pads and adjust their width when vim resizes
+    vim.cmd [[
+        augroup ataraxis
+            autocmd!
+            autocmd BufEnter leftbuffer wincmd l
+            autocmd BufEnter rightbuffer wincmd h
+            autocmd VimResized * call v:lua.adjust_pads()
+        augroup end
+    ]]
 
     -- commands for closing buffers without breaking layout
     vim.cmd('command! AtaraxisClose call v:lua.quit_buffer_or_nvim()')
     vim.cmd('command! AtaraxisForceClose call v:lua.quit_buffer_or_nvim({ "force": 1 })')
-
-    -- adjust pads on resize
-    vim.cmd('autocmd! VimResized * call v:lua.adjust_pads()')
 end
 
 return autocommands
