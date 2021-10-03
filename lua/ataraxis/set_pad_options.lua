937a598b8fb5fef4c69f75d8cc625f08b15b23d9
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-10-03 16:31:18 -0300

    fix: remove sign column from padding windows

--- lua/ataraxis/set_pad_options.lua
+++ lua/ataraxis/set_pad_options.lua
@@ -2,6 +2,7 @@ local function set_pad_options()
     vim.wo.number = false
     vim.wo.relativenumber = false
     vim.wo.cursorline = false
+    vim.wo.signcolumn = 'no'
     vim.bo.buflisted = false
     vim.bo.modifiable = false
     vim.wo.statusline = ' '
