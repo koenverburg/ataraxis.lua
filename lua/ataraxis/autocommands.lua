fab45f0ccc3dbd21704de3f4dffa61b7552f8525
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-10-03 16:56:37 -0300

    refactor!: remove `AtaraxisClose` and `AtaraxisForceClose` autocommands

--- lua/ataraxis/autocommands.lua
+++ lua/ataraxis/autocommands.lua
@@ -5,12 +5,7 @@ require 'ataraxis.adjust_pads'
 vim.cmd [[
     augroup ataraxis
         autocmd!
-        autocmd BufEnter leftbuffer wincmd l
-        autocmd BufEnter rightbuffer wincmd h
+        autocmd BufEnter {leftbuffer,rightbuffer} call v:lua.quit_buffer_or_nvim()
         autocmd VimResized * call v:lua.adjust_pads()
     augroup end
 ]]
-
--- commands for closing buffers without breaking layout
-vim.cmd('command! AtaraxisClose call v:lua.quit_buffer_or_nvim()')
-vim.cmd('command! AtaraxisForceClose call v:lua.quit_buffer_or_nvim({ "force": 1 })')
--- lua/ataraxis/quit_buffer_or_nvim.lua
+++ lua/ataraxis/quit_buffer_or_nvim.lua
@@ -1,17 +1,21 @@
-local get_buffers = require 'helpers.nvim_get_buffers'
-
 function _G.quit_buffer_or_nvim(opts)
     local options = opts or { force = false }
 
     local opened_windows = vim.fn.winnr('$')
-    local visible_buffers = #get_buffers('buflisted(v:val)')
+    local current_buffer_name = vim.fn.bufname('%')
 
     if opened_windows > 3 then
         -- close only split
         vim.cmd(options.force and 'quit!' or 'quit')
-    elseif visible_buffers < 2 then
+    elseif opened_windows < 3 then
         -- close last remaining buffer (alongside pads)
         vim.cmd(options.force and 'quitall!' or 'quitall')
+    elseif current_buffer_name == 'leftbuffer' then
+        -- prevent focusing on left pad
+        vim.cmd('wincmd l')
+    elseif current_buffer_name == 'rightbuffer' then
+        -- prevent focusing on right pad
+        vim.cmd('wincmd h')
     else
         -- close only the current buffer (keep pads)
         vim.cmd('bprevious')
