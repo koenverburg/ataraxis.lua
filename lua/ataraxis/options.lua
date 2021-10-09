516c92a650a0b309b3f91272dd9f7f1a5a98089b
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-10-09 13:20:58 -0300

    style: apply luaformatter code styling rules to all files

--- lua/ataraxis/options.lua
+++ lua/ataraxis/options.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local default_options = {
-    width = 80,
+    width = 80
 }
 
 local options = {}
@@ -9,9 +9,7 @@ local options = {}
 function M.set(user_options)
     local user_fallback_options = user_options or {}
     options = vim.tbl_deep_extend(
-        "force",
-        user_fallback_options,
-        default_options
+        "force", user_fallback_options, default_options
     )
 end
 
