b1b692d5bcd1daff6ac779fcd5244775da0c11c3
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-11-09 20:51:47 -0300

    chore: properly `require` helper functions

--- lua/ataraxis/autocommands.lua
+++ lua/ataraxis/autocommands.lua
@@ -1,11 +1,8 @@
-require "ataraxis.quit_buffer_or_nvim"
-require "ataraxis.adjust_pads"
-
 -- prevent focusing on pads and adjust their width when vim resizes
 vim.cmd [[
     augroup ataraxis
         autocmd!
-        autocmd BufEnter {leftbuffer,rightbuffer} call v:lua.quit_buffer_or_nvim()
-        autocmd VimResized * call v:lua.adjust_pads()
+        autocmd BufEnter {leftbuffer,rightbuffer} lua require('ataraxis.quit_buffer_or_nvim')()
+        autocmd VimResized * lua require('ataraxis.adjust_pads')()
     augroup end
 ]]
