2e7c7a50642d9674839f379d6469d79715eb6e91
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-11-17 22:00:54 -0300

    refactor(adjust_pads): check if pads exist before removing them

--- lua/ataraxis/adjust_pads.lua
+++ lua/ataraxis/adjust_pads.lua
@@ -1,13 +1,19 @@
 local options_store = require "ataraxis.options"
 local add_pads = require "ataraxis.add_pads"
 local set_pad_width = require "ataraxis.set_pad_width"
+local get_buffers = require "helpers.nvim_get_buffers"
+local has_element = require "helpers.has_element"
 
 function _G.adjust_pads()
     local options = options_store.get()
 
     local viewport_too_narrow = vim.o.columns <= options.width
+    local buffers = get_buffers()
+    local left_pad_exists = has_element(buffers, "leftbuffer")
+    local right_pad_exists = has_element(buffers, "rightbuffer")
+    local pads_exists = left_pad_exists or right_pad_exists
 
-    if viewport_too_narrow then
+    if viewport_too_narrow and pads_exists then
         -- remove paddings on shrinking
         vim.cmd "bw! leftbuffer rightbuffer"
     elseif vim.fn.bufwinnr "leftbuffer" == -1 then
